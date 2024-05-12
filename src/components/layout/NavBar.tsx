e53e3a8f1516612b17d8be3b1fd9fd17173f1950
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2024-05-11 20:49:47 -0600

    Nav Bar DONE

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,18 +1,41 @@
 'use client';
 
-import { UserButton} from "@clerk/nextjs";
+import { UserButton, useAuth} from "@clerk/nextjs";
 import Container from "../Container";
 import Link from "next/link";
+import SearchInput from "../SearchInput";
+
+
+
 
 
 export const NavBar = () => {
+    
+    const {userId} =  useAuth();
     return(
         <div className="sticky top-0 border border-b-primary/10 bg-secondary ">
             <Container>
-                <Link href='/' className="font-bold cursor-pointer ">My<span>Hotel</span></Link>
+                <div className="flex justify-between">
+
+                
+                    <Link href='/' className="font-bold cursor-pointer p-2 text-indigo-600">My<span className="">Hotel</span></Link>
+                    <SearchInput />
+                    <div className="flex gap-3 items-center ">
+                        <div>theme</div>
+                        <UserButton afterSignOutUrl="/" />
+                        {
+                            !userId && 
+                            <>
+                                <Link href='/login' className="text-gray-600 text-sm">Login</Link>        
+                                <Link href='/register' className="bg-indigo-500 p-2 rounded-md text-white text-sm hover:bg-indigo-600 transition-all">Register</Link>        
+                            </>
+                        }
+                        
+
+                    </div>
                 
                 
-                <UserButton afterSignOutUrl="/" />
+                </div>
             </Container>
             
         </div>
