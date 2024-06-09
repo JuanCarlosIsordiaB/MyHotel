c49861895067bc2ddd2f87d2e30626c6acc530c6
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2024-06-08 19:45:09 -0600

    Hotel Page DONE

--- src/components/hotel/HotelDetailsClient.tsx
+++ src/components/hotel/HotelDetailsClient.tsx
@@ -18,7 +18,7 @@ const HotelDetailsClient = ({
   hotel,
   bookings,
 }: {
-  hotel: HotelWithRooms,
+  hotel: HotelWithRooms;
   bookings?: Booking[];
 }) => {
   const { getCountryByCode, getStateByCode } = useLocation();
@@ -36,19 +36,31 @@ const HotelDetailsClient = ({
         />
       </div>
       <div>
-        <h3 className="text-2xl font-semibold">{hotel.title}</h3>
-        <div className="font-semibold mt-4">
+        <h3 className="text-2xl font-bold text-indigo-500">{hotel.title}</h3>
+        <div className="font-semibold mt-4 text-gray-500 text-xs mb-9">
           <AmenityItem>
             {" "}
             <MapPin className="h-4 w-4" />
             {country?.name},{state?.name},{hotel.city}
           </AmenityItem>
         </div>
-        <h3 className="font-semibold text-lg mt-4 mb-2">Location Details:</h3>
-        <p className="text-primary/90 mb-2">{hotel.locationDescription}</p>
-        <h3 className="font-semibold text-lg mt-4 mb-2">About this hotel:</h3>
-        <p className="text-primary/90 mb-2">{hotel.description}</p>
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 content-start text-sm">
+        <div className="md:flex mb-2 gap-2">
+          <div className="bg-indigo-500 rounded-md p-2 ">
+            <h3 className="font-bold text-m text-white">
+              Location Details:
+            </h3>
+            <p className=" mb-2 text-xs text-gray-300">{hotel.locationDescription}</p>
+          </div>
+          <div className="bg-indigo-500 rounded-md p-2 mt-1 md:mt-0">
+            <h3 className="font-bold text-m text-white ">About this hotel:</h3>
+            <p className="mb-2 text-xs text-gray-300">{hotel.description}</p>
+          </div>
+
+          
+        </div>
+        <h3 className="text-2xl font-bold text-indigo-500 mt-9">Hotel Amenities</h3>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 content-start text-sm mt-3 bg-indigo-500 p-3 rounded-md text-white">
+            
           {hotel.bar && (
             <AmenityItem>
               <MdLocalBar size={18} /> Bar
@@ -89,7 +101,7 @@ const HotelDetailsClient = ({
         <div>
           {!!hotel.rooms.length && (
             <div>
-              <h3 className="text-lg font-semibold my-4">Hotel Rooms:</h3>
+              <h3 className=" my-4 text-2xl font-bold text-indigo-500 mt-9">Hotel Rooms:</h3>
               <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
                 {hotel.rooms.map((room) => {
                   return (
